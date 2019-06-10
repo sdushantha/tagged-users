628ac6940b72e74912309ae7adbad17ab62d37b0
sdushantha <siddharth.dushantha@gmail.com>
2019-06-10 19:12:30 +0200

    fixed setup.py and added gitignore

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='tagged-users',
-    version='1.0',
+    version='1.1',
     author='Siddharth Dushantha',
     author_email='siddharth.dushantha@gmail.com',
     description='Get all the usernames of the people who got tagged in an Instagram post',
@@ -13,5 +13,6 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='https://github.com/sdushantha/tagged-users',
     py_modules=['tagged-users'],
-    scripts=['tagged-users/tagged-users']
+    scripts=['tagged-users/tagged-users'],
+    install_requires=['requests']
 )
